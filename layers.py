045804c492f7476fd5b52cd7ba2a208db2273c27
stimuli93 <saurabh.arora67@gmail.com>
2016-06-26 23:56:15 +0530

    added softmax layer

--- layers.py
+++ layers.py
@@ -1,59 +1,101 @@
 import numpy as np
 
-class Layer(object):
-    
-    def dense_forward(x,w,b):
-        """
-        Inputs
-        x = numpy array of shape (N,D) where N is number of training examples
-        each having D features
-        w = weights of type numpy array having shape (D,H) where H is the
-        number of output units
-        b = bias of shape (H,) representing bias for output layer
+def dense_forward(x,w,b):
+    """
+    Inputs
+    x = numpy array of shape (N,D) where N is number of training examples
+    each having D features
+    w = weights of type numpy array having shape (D,H) where H is the
+    number of output units
+    b = bias of shape (H,) representing bias for output layer
 
-        Return
-        out = numpy array of shape (N,H) representing the output layer
-        cache = tuple of input parameters used for backpropagation
+    Return
+    out = numpy array of shape (N,H) representing the output layer
+    cache = tuple of input parameters used for backpropagation
 
-        """
-        out = np.dot(x,w) + b
-        cache = (x,w,b)
-        return out,cache
+    """
+    out = np.dot(x,w) + b
+    cache = (x,w,b)
+    return out,cache
 
-    def dense_backward(dout,cache):
-        """
-        Inputs
-        dout = gradient of the output layers having shape (N,H)
-        cache = tuple of input layer, weights & bias
+def dense_backward(dout,cache):
+    """
+    Inputs
+    dout = gradient of the output layers having shape (N,H)
+    cache = tuple of input layer, weights & bias
 
-        Return
-        dx = numpy array of shape (N,D) representing gradients of input layer
-        dw = numpy array of shape (D,H) representing gradients of weights
-        db = numpy array of shape (H,) representing gradients of bias
-        """
-        x,w,b = cache
-        dx = np.dot(dout,w.T)
-        dw = np.dot(x.T,dout)
-        db = np.sum(dout,axis=0)
-        return dx,dw,db
+    Return
+    dx = numpy array of shape (N,D) representing gradients of input layer
+    dw = numpy array of shape (D,H) representing gradients of weights
+    db = numpy array of shape (H,) representing gradients of bias
+    """
+    x,w,b = cache
+    dx = np.dot(dout,w.T)
+    dw = np.dot(x.T,dout)
+    db = np.sum(dout,axis=0)
+    return dx,dw,db
 
-    def relu_forward(x):
-        """
-        Inputs
-        x = numpy array of shape (N,D) representing input layer
+def relu_forward(x):
+    """
+    Inputs
+    x = numpy array of shape (N,D) representing input layer
 
-        Return
-        out = numpy array of shape (N,D) representing output of relu layer
-        cache = storing x for backpropagation
-        """
-        out = x*(x>0)
-        cache = x
-        return out,cache
+    Return
+    out = numpy array of shape (N,D) representing output of relu layer
+    cache = storing x for backpropagation
+    """
+    out = x*(x>0)
+    cache = x
+    return out,cache
 
-    def relu_backward(dout,cache):
-        x = cache
-        dx = dout*(x>0)
-        return dx
+def relu_backward(dout,cache):
+    """
+    Inputs
+    dout = numpy array of shape (N,D) representing gradients of output
+    layer
+    cache = numpy array of shape (N,D) representing input layer for
+    backpropagation
+
+    Return
+    dx = numpy array of shape (N,D) representing gradients of input layer
+    """
+    x = cache
+    dx = dout*(x>0)
+    return dx
 
 
+def softmax_loss_forward(x,y):
+    """
+    Inputs
+    x = numpy array of shape (N,C) representing input layer
+    y = numpy array of shape (N,) representing labels of each training
+    example
 
+    Returns
+    loss = softmax loss
+    cache = tuple of (x,y) used for backpropagation
+    """
+    cache = (x,y)
+    x_exp = np.exp((x.T - np.max(x,axis=1)).T)
+    probabilities = (x_exp.T / np.sum(x_exp,axis=1)).T
+    N = x.shape[0]
+    loss = -np.mean(np.log(probabilities[xrange(N),y]))
+    return loss,cache
+
+def softmax_loss_backward(dout,cache):
+    """
+    Inputs
+    dout - Gradient coming from 1 layer deeper
+    cache - (x,y) used for backpropagation
+
+    Return
+    dx = numpy array of shape (N,C) representing gradients of input layer
+    """
+    x,y = cache
+    x_exp = np.exp((x.T - np.max(x,axis=1)).T)
+    probabilities = (x_exp.T / np.sum(x_exp,axis=1)).T
+    N = x.shape[0]
+    dx = probabilities
+    dx[xrange(N),y] -= 1
+    dx = dx * dout
+    return dx
