1f26ad9e72a88aa3943220ffa37da2e074fd6739
stimuli93 <saurabh.arora67@gmail.com>
2016-07-03 15:04:06 +0530

    made 2 hidden layer Neural Network from 1 hidden Layer Neural Network

--- models.py
+++ models.py
@@ -5,19 +5,23 @@ import numpy as np
 class MLP(object):
 
     """
-    1 hidden layer neural network with softmax classifier
+    2 hidden layer neural network with softmax classifier
     """
-    def __init__(self, inputSize, hiddenSize, outputSize, hiddenLayer='relu'):
+    def __init__(self, inputSize, hiddenSize1,hiddenSize2, outputSize, hiddenLayer='relu'):
         self.inputSize = inputSize
-        self.hiddenSize = hiddenSize
+        self.hiddenSize1 = hiddenSize1
+        self.hiddenSize2 = hiddenSize2
         self.outputSize = outputSize
         self.hiddenLayer = hiddenLayer
-        self.W1 = np.random.random(size=(inputSize, hiddenSize)) - 0.5
-        self.b1 = np.random.random(size=(hiddenSize)) - 0.5
-        self.W2 = np.random.random(size=(hiddenSize, outputSize)) - 0.5
-        self.b2 = np.random.random(size=(outputSize)) - 0.5
+        self.W1 = np.random.random(size=(inputSize, hiddenSize1)) - 0.5
+        self.b1 = np.random.random(size=(hiddenSize1)) - 0.5
+        self.W2 = np.random.random(size=(hiddenSize1, hiddenSize2)) - 0.5
+        self.b2 = np.random.random(size=(hiddenSize2)) - 0.5
+        self.W3 = np.random.random(size=(hiddenSize2,outputSize)) - 0.5
+        self.b3 = np.random.random(size=(outputSize)) - 0.5
         self.loss_history = []
-        self.gradient_history = []
+        self.gradientLayer1 = []
+        self.gradientLayer2 = []
     
 
     def train(self, X, y, X_val=None, y_val=None, learning_rate=1e-2,reg = 1e-4,decay_rate=0.95,
@@ -31,9 +35,13 @@ class MLP(object):
             l2out, l2cache = layers.non_linearity_forward(l1out,self.hiddenLayer)
             # adding dense layer3
             l3out, l3cache = layers.dense_forward(l2out, self.W2, self.b2)
+            #adding non-linearity layer4
+            l4out,l4cache = layers.non_linearity_forward(l3out,self.hiddenLayer)
+            #adding dense layer5
+            l5out,l5cache = layers.dense_forward(l4out,self.W3,self.b3)
             # adding softmax layer
-            loss, l4cache = layers.softmax_loss_forward(l3out, y[ids])
-            loss = loss + reg*(np.sum(self.W1**2) + np.sum(self.W2**2))
+            loss, l6cache = layers.softmax_loss_forward(l5out, y[ids])
+            loss = loss + reg*(np.sum(self.W1**2) + np.sum(self.W2**2) + np.sum(self.W3**2))
             self.loss_history.append(loss)
             if verbose and i % 500 == 0:
             	lr = lr*0.95
@@ -41,23 +49,30 @@ class MLP(object):
                 if X_val is not None and y_val is not None:
                     print "Validation Accuracy :%f"%(self.accuracy(X_val,y_val))
 
-            dlayer4 = 1.0
-            dlayer3 = layers.softmax_loss_backward(dlayer4, l4cache)
-            dlayer2, dW2, db2 = layers.dense_backward(dlayer3, l3cache)
-            dlayer1 = layers.non_linearity_backward(dlayer2, l2cache,self.hiddenLayer)
+            dlayer6 = 1.0
+            dlayer5 = layers.softmax_loss_backward(dlayer6, l6cache)
+            dlayer4, dW3, db3 = layers.dense_backward(dlayer5, l5cache)
+            dlayer3 = layers.non_linearity_backward(dlayer4, l4cache,self.hiddenLayer)
+            dlayer2, dW2, db2 = layers.dense_backward(dlayer3,l3cache)
+            dlayer1 = layers.non_linearity_backward(dlayer2,l2cache,self.hiddenLayer)
             _, dW1, db1 = layers.dense_backward(dlayer1, l1cache)
 
-            self.gradient_history.append(np.sum(np.abs(dlayer1)))
+            self.gradientLayer1.append(np.mean(np.abs(dlayer1)))
+            self.gradientLayer2.append(np.mean(np.abs(dlayer3)))
             self.W1 = self.W1 - lr * dW1 + 0.5*reg*dW1
             self.b1 = self.b1 - lr * db1
             self.W2 = self.W2 - lr * dW2 + 0.5*reg*dW2
             self.b2 = self.b2 - lr * db2
+            self.W3 = self.W3 - lr * dW3 + 0.5*reg*dW3
+            self.b3 = self.b3 - lr * db3
 
     def predict(self, X):
         l1out, _ = layers.dense_forward(X, self.W1, self.b1)
         l2out, _ = layers.non_linearity_forward(l1out,self.hiddenLayer)
        	l3out, _ = layers.dense_forward(l2out, self.W2, self.b2)
-       	return np.argmax(l3out, axis=1)
+        l4out, _ = layers.non_linearity_forward(l3out,self.hiddenLayer)
+        l5out, _ = layers.dense_forward(l4out, self.W3, self.b3)
+       	return np.argmax(l5out, axis=1)
 
     def accuracy(self, X, y):
        	return np.mean(self.predict(X) == y)
