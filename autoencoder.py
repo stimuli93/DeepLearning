0332398361167da0ef512fe048b400c848869466
stimuli93 <saurabh.arora67@gmail.com>
2016-07-22 00:55:04 +0530

    added getloss() for SimpleAutoEncoder class

--- autoencoder.py
+++ autoencoder.py
@@ -18,17 +18,19 @@ class SimpleAutoEncoder(object):
         self.W2 = initializations.xavier_init(shape=(hiddenSize, inputSize), hiddenLayer='sigmoid')
         self.b2 = initializations.uniform_init(shape=(inputSize,))
         self.params = {}
+        self.reg = 1e-5
 
-    def train(self, X, learning_rate=1e-3, reg=1e-4, decay_rate=1.00, opt='sgd', n_iters=1000,
+    def train(self, X, y, learning_rate=1e-3, reg=1e-4, decay_rate=1.00, opt='sgd', n_iters=1000,
               batch_size=200, verbose=True):
         lr = learning_rate
+        self.reg = reg
         for i in range(n_iters):
             ids = np.random.choice(X.shape[0], batch_size)
             layer1, l1cache = layers.dense_forward(X[ids], self.W1, self.b1)
             layer2, l2cache = layers.non_linearity_forward(layer1, hiddenLayer='relu')
             layer3, l3cache = layers.dense_forward(layer2, self.W2, self.b2)
             layer4, l4cache = layers.non_linearity_forward(layer3, hiddenLayer='sigmoid')
-            loss, l5cache = layers.binary_cross_entropy_loss_forward(layer4, X[ids])
+            loss, l5cache = layers.binary_cross_entropy_loss_forward(layer4, y[ids])
 
             dlayer5 = 1.0
             dlayer4 = layers.binary_cross_entropy_loss_backward(dlayer5, l5cache)
@@ -59,3 +61,12 @@ class SimpleAutoEncoder(object):
         l3, _ = layers.dense_forward(l2, self.W2, self.b2)
         l4, _ = layers.non_linearity_forward(l3, hiddenLayer='sigmoid')
         return l4
+
+    def getloss(self, X, y):
+        layer1, _ = layers.dense_forward(X, self.W1, self.b1)
+        layer2, _ = layers.non_linearity_forward(layer1, hiddenLayer='relu')
+        layer3, _ = layers.dense_forward(layer2, self.W2, self.b2)
+        layer4, _ = layers.non_linearity_forward(layer3, hiddenLayer='sigmoid')
+        loss, _ = layers.binary_cross_entropy_loss_forward(layer4, y)
+        loss += 0.5 * self.reg * (np.sum(self.W1 * self.W1) + np.sum(self.W2 * self.W2))
+        return loss
