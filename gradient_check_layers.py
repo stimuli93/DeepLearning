f0e5d3ab8078800e19630ddc9a33917cb4886d03
stimuli93 <saurabh.arora67@gmail.com>
2016-07-24 22:53:11 +0530

    gradient check for softmax_loss & binary_cross_entropy loss

--- gradient_check_layers.py
+++ gradient_check_layers.py
@@ -1,30 +1,63 @@
 import numpy as np
 import layers
 
+
 def rel_error(x, y):
-  """ returns relative error """
-  return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
+    """ returns relative error """
+    return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
+
 
 def eval_numerical_gradient_array(f, x, df, h=1e-5):
-  """
-  Evaluate a numeric gradient for a function that accepts a numpy
-  array and returns a numpy array.
-  """
-  grad = np.zeros_like(x)
-  it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
-  while not it.finished:
-    ix = it.multi_index
+    """
+    Evaluate a numeric gradient for a function that accepts a numpy
+    array and returns a numpy array.
+    """
+    grad = np.zeros_like(x)
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        ix = it.multi_index
 
-    oldval = x[ix]
-    x[ix] = oldval + h
-    pos = f(x).copy()
-    x[ix] = oldval - h
-    neg = f(x).copy()
-    x[ix] = oldval
+        oldval = x[ix]
+        x[ix] = oldval + h
+        pos = f(x).copy()
+        x[ix] = oldval - h
+        neg = f(x).copy()
+        x[ix] = oldval
 
-    grad[ix] = np.sum((pos - neg) * df) / (2 * h)
-    it.iternext()
-  return grad
+        grad[ix] = np.sum((pos - neg) * df) / (2 * h)
+        it.iternext()
+    return grad
+
+
+def eval_numerical_gradient(f, x, verbose=True, h=0.00001):
+    """
+    a naive implementation of numerical gradient of f at x
+    - f should be a function that takes a single argument
+    - x is the point (numpy array) to evaluate the gradient at
+    """
+
+    fx = f(x) # evaluate function value at original point
+    grad = np.zeros_like(x)
+    # iterate over all indexes in x
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+
+        # evaluate function at x+h
+        ix = it.multi_index
+        oldval = x[ix]
+        x[ix] = oldval + h # increment by h
+        fxph = f(x) # evalute f(x + h)
+        x[ix] = oldval - h
+        fxmh = f(x) # evaluate f(x - h)
+        x[ix] = oldval # restore
+
+        # compute the partial derivative with centered formula
+        grad[ix] = (fxph - fxmh) / (2 * h) # the slope
+        if verbose:
+            print ix, grad[ix]
+        it.iternext() # step to next dimension
+
+    return grad
 
 
 def test_denselayer():
@@ -59,6 +92,7 @@ def test_relulayer():
     print 'Testing relu layers:'
     print 'dx error: ', rel_error(dx_num, dx)
 
+
 def test_tanhlayer():
     x = np.random.randn(10, 10)
     dout = np.random.randn(*x.shape)
@@ -70,6 +104,7 @@ def test_tanhlayer():
     # The error should be around 1e-12
     print 'Testing tanh layers:'
     print 'dx error: ', rel_error(dx_num, dx)
+
 
 def test_sigmoidlayer():
     x = np.random.randn(10, 10)
@@ -84,9 +119,39 @@ def test_sigmoidlayer():
     print 'dx error: ', rel_error(dx_num, dx)
 
 
+def test_softmax_loss():
+    num_classes, num_inputs = 10, 50
+    x = 0.001 * np.random.randn(num_inputs, num_classes)
+    y = np.random.randint(num_classes, size=num_inputs)
+    cache = (x, y)
+    dout = 1.0
+
+    dx_num = eval_numerical_gradient(lambda x: layers.softmax_loss_forward(x, y)[0], x, verbose=False)
+    dx = layers.softmax_loss_backward(dout, cache)
+
+    # Test svm_loss function. Loss should be around 9 and dx error should be 1e-9
+    print 'Testing softmax_loss:'
+    print 'dx error: ', rel_error(dx_num, dx)
+
+
+def test_binary_cross_entropy_loss():
+    x = np.random.rand(10, 10)
+    y = np.random.rand(*x.shape) < 0.5
+    cache = (x, y)
+    dout = 1.0
+
+    dx_num = eval_numerical_gradient_array(lambda x: layers.binary_cross_entropy_loss_forward(x, y)[0], x, dout)
+    dx = layers.binary_cross_entropy_loss_backward(dout, cache)
+
+    # Test svm_loss function. Loss should be around 9 and dx error should be 1e-9
+    print 'Testing binary cross-entropy loss:'
+    print 'dx error: ', rel_error(dx_num, dx)
+
 
 if __name__ == '__main__':
     test_denselayer()
     test_relulayer()
     test_tanhlayer()
     test_sigmoidlayer()
+    test_softmax_loss()
+    test_binary_cross_entropy_loss()
