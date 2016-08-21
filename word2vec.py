2b8fed76d64fc8933a356a998a9c93ba8fdbfa72
stimuli93 <saurabh.arora67@gmail.com>
2016-08-21 22:42:21 +0530

    added stopword removal & few architectural changes

--- word2vec.py
+++ word2vec.py
@@ -5,6 +5,7 @@ import initializations
 import pickle
 import operator
 from collections import Counter
+from nltk.corpus import stopwords
 
 
 def normalise_vector(x):
@@ -65,8 +66,9 @@ class Word2Vec:
                 word_counter[word] += 1
 
         itr = 3
+        stop_words = set(stopwords.words('english'))
         for key, value in word_counter.items():
-            if value > self.min_count:
+            if key not in stop_words and value >= self.min_count:
                 self.index_to_word[itr] = key
                 self.word_to_index[key] = itr
                 itr += 1
@@ -84,50 +86,65 @@ class Word2Vec:
         :param X: list of sentences used for training
         """
         N = len(X)
+        id_x = []
+        for i in xrange(N):
+            id_x.append(self.word_to_index[self.start_token])
+            sentence = nltk.word_tokenize(X[i])
+            for word in sentence:
+                if word in self.word_to_index:
+                    id_x.append(self.word_to_index[word])
+            id_x.append(self.word_to_index[self.start_token])
+
+        corpus_size = len(id_x)
         left_window = self.window
         right_window = self.window
         for itr in xrange(n_iters):
-            batch = np.random.randint(N, size=batch_size)
-            for b_id in batch:
-                sentence = '%s %s %s' % (self.start_token, X[b_id], self.end_token)
-                word_list = nltk.word_tokenize(sentence)
-                word_idx = [self.word_to_index.get(wd, 2) for wd in word_list]
-                word_list_len = len(word_idx)
+            batch = np.random.randint(corpus_size, size=batch_size)
+            for w_id in batch:
                 loss = 0.0
-                for i in xrange(word_list_len):
-                    context_window = word_idx[max(0, i-left_window):i] +\
-                                     word_idx[i+1:min(i+1+right_window, word_list_len)]
-                    loss += self.train_word_in_context(word_idx[i], context_window, learning_rate)
-                self.loss_history.append(loss/word_list_len)
+                context_window = id_x[max(0, w_id-left_window):w_id] +\
+                                 id_x[w_id+1:min(w_id+1+right_window, corpus_size)]
 
-    def train_word_in_context(self, word_id, context_window, learning_rate):
+                # Negative sampling
+                neg_samples_count = 5
+                neg_samples = np.random.randint(corpus_size, size=neg_samples_count)
+                neg_samples[0] = w_id
+
+                neg_samples = [id_x[sample] for sample in neg_samples]
+
+                loss += self.train_word_in_context(id_x[w_id], np.array(context_window),
+                                                   np.array(neg_samples), learning_rate)
+                self.loss_history.append(loss)
+
+    def train_word_in_context(self, word_id, context_window, neg_samples, learning_rate):
         """
         :param learning_rate:
         :param word_id: the index of word to predicted
         :param context_window: list of word_ids in the context of given word
+        :param neg_samples: list of word_ids choosen as negative samples
         """
+
         # mean of self.W_inp of words in context_window is used as input
         x_inp = np.mean(self.W_inp[context_window, :], axis=0)
-
-        # Negative sampling
-        neg_samples_count = 5
-        neg_samples = np.random.randint(self.vocab_size, size=neg_samples_count)
-
-        if word_id not in neg_samples:
-            neg_samples[0] = word_id
-        
-        W = self.W_out[neg_samples, :]
         x_inp = x_inp.reshape((1, self.size))
-        b = np.zeros((1, neg_samples_count))
+
+        neg_sample_count = len(neg_samples)
+        b = np.zeros((1, neg_sample_count))
         y = [0]
-        y[0] = neg_samples.tolist().index(word_id)
+        output = np.random.randint(neg_sample_count, size=1)
+        y[0] = output
+        neg_samples[0] = neg_samples[output]
+        neg_samples[output] = word_id
         y = np.array(y)
+        W = self.W_out[neg_samples, :]
 
         layer1, l1cache = layers.dense_forward(x_inp, W.T, b)
-        loss, l2cache = layers.softmax_loss_forward(layer1, y)
+        layer2, l2cache = layers.sigmoid_forward(layer1)
+        loss, l3cache = layers.softmax_loss_forward(layer2, y)
 
-        dlayer2 = 1.0
-        dlayer1 = layers.softmax_loss_backward(dlayer2, l2cache)
+        dlayer3 = 1.0
+        dlayer2 = layers.softmax_loss_backward(dlayer3, l3cache)
+        dlayer1 = layers.sigmoid_backward(dlayer2, l2cache)
         dx_inp, dW_tmp, db = layers.dense_backward(dlayer1, l1cache)
         dW = dW_tmp.T
 
